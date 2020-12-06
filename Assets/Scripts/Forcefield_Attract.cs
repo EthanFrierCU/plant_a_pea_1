87eccdd5c95128a24b2bc4076a55b9afc4727f36
EthanFrier <etfr1107@colorado.edu>
2020-12-06 16:44:31 -0500

    debug text working

--- Assets/Scripts/Forcefield_Attract.cs
+++ Assets/Scripts/Forcefield_Attract.cs
@@ -5,7 +5,7 @@ using CUVR;
 
 public class Forcefield_Attract : MonoBehaviour
 {
-    public Transform pea; 
+    public Transform pea;
     public float speed = 1;
     Vector3 targetPosition;
     bool inForcefield;
@@ -13,41 +13,35 @@ public class Forcefield_Attract : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // pea = GetComponent<Rigidbody>();
-        targetPosition = new Vector3(0f,-0.5f,1f);
+        targetPosition = new Vector3(0f, -0.5f, 1f);
         inForcefield = false;
     }
 
     void OnTriggerEnter(Collider other)
     {
         other.transform.gameObject.GetComponent<Interactable>().enabled = false;
-       if (other.CompareTag("PeaSeed"))
+        if (other.CompareTag("PeaSeed"))
         {
             other.transform.parent = null;
             other.transform.parent = this.transform;
             inForcefield = true;
             pea = other.transform;
-
         }
     }
 
-    void Update ()
+    void Update()
     {
-        if(inForcefield)
+        if (inForcefield)
         {
-            float step =  speed * Time.deltaTime;
-            pea.transform.position = Vector3.MoveTowards(pea.transform.position, targetPosition, step);   
-            
+            float step = speed * Time.deltaTime;
+            pea.transform.position = Vector3.MoveTowards(pea.transform.position, targetPosition, step);
+
             if (Vector3.Distance(pea.transform.position, targetPosition) < 0.001f)
             {
-            inForcefield = false;
-            }     
+                inForcefield = false;
+            }
         }
-        
-        // if(inForcefield)
-        // {
-        //     pea.position = targetPosition;
-        // }
+
     }
-    
+
 }
