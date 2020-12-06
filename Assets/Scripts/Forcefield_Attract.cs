fdc08de7fbc3077b2043d4b57b32887a548b582f
EthanFrier <etfr1107@colorado.edu>
2020-12-06 11:42:32 -0500

    working on trigger zone

---
+++ Assets/Scripts/Forcefield_Attract.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Forcefield_Attract : MonoBehaviour
+{
+    public Rigidbody pea; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pea = GetComponent<Rigidbody>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "PeaSeed")
+        {
+            other.transform.SetParent(null);
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        //PeaSeed moves towards center uning rigidbody moveposition
+    }
+    
+}
