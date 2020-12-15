69acf26a9e554081fb032d5d4e4ea636297ba8dd
EthanFrier <etfr1107@colorado.edu>
2020-12-15 17:40:05 -0500

    one more commit to be safe

---
+++ Assets/Scripts/playSound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playSound : MonoBehaviour
+{
+
+    public AudioSource Bell;
+
+    public void PlayBell()
+    {
+
+        Bell.Play();
+    }
+}
